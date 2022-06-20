8e845f5a4ea910dfd1586454c96cfde2e9dbf506
Rahul Anil Gaikwad <47887506+rahulgaikwad97@users.noreply.github.com>
2022-06-20 11:10:33 -0500

    final commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,28 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import profile_pic from "../assets/profile_pic.jpg";  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
-            <img
+            <img 
               className="profile_image"
-              src={profile_pic}
+              src= {profile_pic}
               alt="Profile Pic"
-            ></img>
+              ></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Rahul Gaikwad</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              Hey there! I am Rahul Gaikwad. I am currently pursuing a master's degree in Information Technology and Management from the University of Texas at Dallas. I am a passionate learner and love to explore new things. In my free time, I love to play chess and keep myself updated on the new emerging technology.
             </div>
           </div>
-        </div>
+        </div> 
       </div>
-    );
+    )
   }
 }
